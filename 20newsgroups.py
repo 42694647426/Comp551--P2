86af08760dff880b205321cc8b48395cbd3c90ea
Anna <hyyshao@gmail.com>
2020-03-09 21:31:37 -0400

    Add logistic regression and decision tree in 20newsgroups.py

--- 20newsgroups.py
+++ 20newsgroups.py
@@ -4,6 +4,12 @@ from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.feature_extraction.text import TfidfTransformer
 from sklearn.svm import LinearSVC
 from sklearn.pipeline import Pipeline
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LogisticRegression
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.model_selection import cross_validate
+
 
 # Get training data from the 20 news group dataset
 twenty_train = fetch_20newsgroups(subset='train', shuffle=True, random_state=42, remove=(['headers', 'footers', 'quotes']))
@@ -24,14 +30,32 @@ X_train_tfidf.shape
 twenty_test = fetch_20newsgroups(subset='test', shuffle=True, random_state=42)
 docs_test = twenty_test.data
 
-#pipeline for SVC
-text_clf = Pipeline([
+#pipeline for logistic regression
+text_logreg = Pipeline([
  ('vect', CountVectorizer()),
  ('tfidf', TfidfTransformer()),
-  ('clf', LinearSVC(loss='hinge', penalty='l2',
-          random_state=42, max_iter=5, tol=None))
+  ('logreg', LogisticRegression(verbose=1, solver='liblinear',random_state=0, C=5, penalty='l2',max_iter=1000))
 ])
 
-text_clf.fit(twenty_train.data, twenty_train.target)
-predicted = text_clf.predict(docs_test)
-np.mean(predicted == twenty_test.target)
+text_logreg.fit(twenty_train.data, twenty_train.target)
+predicted = text_logreg.predict(docs_test)
+accuracy = np.mean(predicted == twenty_test.target)
+
+print("Logistic Regression accuracy:", accuracy)
+#cross validation
+scores = cross_validate(text_logreg, twenty_train.data, twenty_train.target)
+print("Logistic Regression cross validation:", scores)
+
+#Decision tree
+text_tree = Pipeline([
+ ('vect', CountVectorizer()),
+ ('tfidf', TfidfTransformer()),
+  ('tree', DecisionTreeClassifier())
+])
+text_tree.fit(twenty_train.data, twenty_train.target)
+predicted = text_tree.predict(docs_test)
+accuracy = np.mean(predicted == twenty_test.target)
+print("Decision Tree Classifier accuracy:", accuracy)
+
+scores = cross_validate(text_logreg, twenty_train.data, twenty_train.target)
+print("Decision Tree Classifier cross validation:", scores)
