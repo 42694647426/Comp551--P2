e60dff07b2c298c870d9a3a07763ac8bdf71f527
Anna <hyyshao@gmail.com>
2020-03-10 14:14:18 -0400

    logistic regression C parameter range and try to add plots

--- 20newsgroups.py
+++ 20newsgroups.py
@@ -13,13 +13,22 @@ from sklearn.ensemble import AdaBoostClassifier
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.model_selection import cross_validate
 from sklearn.model_selection import GridSearchCV
+import matplotlib.pyplot as plt
+import pandas as pd
 
 
 
 # Get training data from the 20 news group dataset
 twenty_train = fetch_20newsgroups(subset='train', shuffle=True, random_state=42, remove=(['headers', 'footers', 'quotes']))
+#print(twenty_train.target_names)
 
-print(twenty_train.target_names)
+#plot dataset(a problem with dataset)
+'''
+df = pd.DataFrame(twenty_train)
+fig = plt.figure(figsize=(8,6))
+df.groupby('target_names').Consumer_complaint_narrative.count().plot.bar(ylim=0)
+plt.show()
+'''
 
 # Tokenize text by indexing each word in each document and associate it with its occurrence
 count_vect = CountVectorizer()
@@ -34,6 +43,7 @@ twenty_test = fetch_20newsgroups(subset='test', shuffle=True, random_state=42)
 docs_test = twenty_test.data
 
 #fit data with different models using pipelines and forloop
+
 pipeline = Pipeline([
  ('vect', CountVectorizer()),
  ('tfidf', TfidfTransformer()),
@@ -41,15 +51,16 @@ pipeline = Pipeline([
 ])
 clfs = []
 
+
+clfs.append(LogisticRegression()) #c
 clfs.append(DecisionTreeClassifier())
 clfs.append(LinearSVC())#c
-clfs.append(LogisticRegression()) #c
-
 clfs.append(AdaBoostClassifier()) #learning rate
 clfs.append(RandomForestClassifier())# n_estimators
 
 
 '''
+print("Test for different models:................................................")
 for classifier in clfs:
     pipeline.set_params(clf = classifier)
     pipeline.fit(twenty_train.data, twenty_train.target)
@@ -67,12 +78,19 @@ for classifier in clfs:
 '''
 
 #find best parameters
+print("Tuning hyperparameters for the models:..............................................")
 for classifier in clfs:
     pipeline.set_params(clf=classifier)
-    if(isinstance(classifier, LogisticRegression) or isinstance(classifier, LinearSVC) ):
+    if(isinstance(classifier, LinearSVC) ):
       cv_grid = GridSearchCV(pipeline, param_grid={
       'clf__C': np.linspace(0.01, 1.5, 3)
+
      })
+    elif (isinstance(classifier, LogisticRegression)  ):
+        cv_grid = GridSearchCV(pipeline, param_grid={
+            'clf__C': [0.1,1,10,100,1000] ,# logistic regression has a problem
+            'clf__max_iter': [4000]
+        })
     elif isinstance(classifier, AdaBoostClassifier):
       cv_grid = GridSearchCV(pipeline, param_grid={
       'clf__learning_rate': np.linspace(0.1, 1.5, 3)
@@ -90,8 +108,9 @@ for classifier in clfs:
     print("-----------------------------------------------------------------")
     print(model_name)
     print(model_name + " best parameter: " + str(best_par))
-    print(model_name + " best estimator: "+ str(best_estimator))
+    print(model_name + " best estimator: "+ str(best_estimator.steps[2]))
     print(model_name + " best CV score: "+ str(best_score))
     y_predict = cv_grid.predict(docs_test)
-    accuracy = accuracy_score(twenty_test.target, y_predict) # a problem with datasets
+    accuracy = accuracy_score(twenty_test.target, y_predict)
     print('Accuracy of the best ' + model_name + 'after CV is %.6f%%' % (accuracy * 100))
+
