443064050091f07ae9dff9620bdcfa32dbd1998f
deduction000 <60801705+deduction000@users.noreply.github.com>
2020-03-12 17:41:36 -0400

    Add files via upload

--- 20newsgroups.py
+++ 20newsgroups.py
@@ -17,8 +17,32 @@ from sklearn.model_selection import GridSearchCV
 import matplotlib.pyplot as plt
 import pandas as pd
 
+
+# TO DELETE
+categories = ['alt.atheism',
+ 'comp.graphics',
+ 'comp.os.ms-windows.misc',
+ 'comp.sys.ibm.pc.hardware',
+ 'comp.sys.mac.hardware',
+ 'comp.windows.x',
+ 'misc.forsale',
+ 'rec.autos',
+ 'rec.motorcycles',
+ 'rec.sport.baseball',
+ 'rec.sport.hockey',
+ 'sci.crypt',
+ 'sci.electronics',
+ 'sci.med',
+ 'sci.space',
+ 'soc.religion.christian',
+ 'talk.politics.guns',
+ 'talk.politics.mideast',
+ 'talk.politics.misc',
+ 'talk.religion.misc']
+
+
 # Get training data from the 20 news group dataset
-twenty_train = fetch_20newsgroups(subset='train', shuffle=True, random_state=42,
+twenty_train = fetch_20newsgroups(subset='train', shuffle=True, random_state=42, categories = categories,
                                   remove=(['headers', 'footers', 'quotes']))
 # print(twenty_train.target_names)
 
@@ -33,25 +57,34 @@ plt.xticks(range(20), twenty_train.target_names)
 plt.xticks(rotation=30)
 plt.show()
 
+# Preprocessing method:
+pre_method = input("Preprocessing method (1) tf-idf; (2) tf:    ")
+
 # Tokenize text by indexing each word in each document and associate it with its occurrence
 count_vect = CountVectorizer()
 X_train_counts = count_vect.fit_transform(twenty_train.data)
-
-# Transform occurrence into tf-idf
-tfidf_transformer = TfidfTransformer()
-X_train_tfidf = tfidf_transformer.fit_transform(X_train_counts)
+X_train = 0
 
 # test set
-twenty_test = fetch_20newsgroups(subset='test', shuffle=True, random_state=42)
+twenty_test = fetch_20newsgroups(subset='test', shuffle=True, random_state=42, categories = categories)
 docs_test = twenty_test.data
 print("Analyzing 20 News Groups dataset .......................................................")
 # fit data with different models using pipelines and forloop
 
-pipeline = Pipeline([
+
+if pre_method == '2':
+    pipeline = Pipeline([
+    ('vect', CountVectorizer()),
+    ('tfidf', TfidfTransformer(use_idf = False)),
+    ('clf', LogisticRegression())  # step2 - classifier
+    ])
+else:
+    pipeline = Pipeline([
     ('vect', CountVectorizer()),
     ('tfidf', TfidfTransformer()),
     ('clf', LogisticRegression())  # step2 - classifier
-])
+    ])
+
 
 clfs = []
 
@@ -69,6 +102,9 @@ best_score = 0
 print("Test for different models:................................................")
 for classifier in clfs:
     pipeline.set_params(clf = classifier)
+   # if pre_method == '2':
+    #    pipeline.set_params(TfidfTransformer(use_idf = False))
+    
     pipeline.fit(twenty_train.data, twenty_train.target)
     predicted = pipeline.predict(docs_test)
     accuracy = np.mean(predicted == twenty_test.target)
@@ -100,46 +136,46 @@ for classifier in clfs:
     param_name = ""
     if (isinstance(classifier, LinearSVC)):
         cv_grid = GridSearchCV(pipeline, param_grid={
-            #'clf__penalty': ['l1', 'l2'],
-            #'clf__loss': ['hinge', 'squared_hinge'],
+            'clf__penalty': ['l1', 'l2'],
+            'clf__loss': ['squared_hinge'],
             'clf__dual': [False],
-            'clf__C': [0.01, 0.1, 1],#0.7?
+            'clf__C':np.linspace(0,1,20),#0.7?
             'clf__random_state': [42]
-        }, cv=3)
+        }, cv=10)
         param_name = 'clf__C'
     elif (isinstance(classifier, LogisticRegression)):
         cv_grid = GridSearchCV(pipeline, param_grid={
-            #'clf__penalty': ['l1', 'l2'],
-            #'clf__dual': [False],
-            'clf__C': [0.01, 0.1, 1] #1000
-            #'clf__solver': ['liblinear', 'saga'],
-            #'clf__max_iter':[4000],
-            #'clf__random_state': [42]
-        }, cv=3)
+            'clf__penalty': ['l1', 'l2'],
+            'clf__dual': [False],
+            'clf__C': [0.01, 0.1, 1, 10, 100, 1000], #1000
+            'clf__solver': ['liblinear', 'saga'],
+            'clf__max_iter':[1000000],
+            'clf__random_state': [42]
+        }, cv=10)
         param_name = 'clf__C'
+        
     elif isinstance(classifier, AdaBoostClassifier):
         cv_grid = GridSearchCV(pipeline, param_grid={
-            #'clf__n_estimators': [50, 100, 500],
-            'clf__learning_rate': [0.01, 0.1, 1.],#0.5
-            'clf__random_state': [42]
-            # 'clf__n_estimators': [200,400,1000] # try it after finding best lr
+            'clf__learning_rate': np.linspace(0,1,20),#0.5
+            'clf__random_state': [42],
+            'clf__n_estimators': [200,400,1000] # try it after finding best lr
         }, cv=10)
         param_name = 'clf__learning_rate'
     elif isinstance(classifier, RandomForestClassifier):
         cv_grid = GridSearchCV(pipeline, param_grid={
             'clf__n_estimators': [10, 50, 100],
-            #'clf__criterion': ['gini', 'entropy'],
-            #'clf__max_features': ['auto', 'log2'],
-            #'clf__max_depth': [None, 10, 100],
-            #'clf__max_leaf_nodes': [None, 100, 500],
-            'clf__random_state': [42]
-            # 'clf_max_depth': [10, 20, 30, 40, 50, 60, 70, 80, 90, 100, None] #try it later
+            'clf__criterion': ['gini', 'entropy'],
+            'clf__max_features': ['auto', 'log2'],
+            'clf__max_depth': [None, 10, 100],
+            'clf__max_leaf_nodes': [None, 100, 500],
+            'clf__random_state': [42],
+            'clf_max_depth': [10, 20, 40, 60, 80, 100, None] #try it later
         }, cv=10)
         param_name = 'clf__n_estimators'
     elif isinstance(classifier, DecisionTreeClassifier):
         cv_grid = GridSearchCV(pipeline, param_grid={
-            #'clf__criterion': ['gini', 'entropy'],
-            #'clf__max_depth': [None, 10, 100],
+            'clf__criterion': ['gini', 'entropy'],
+            'clf__max_depth': [None, 10, 100],
             'clf__max_leaf_nodes': [None, 100, 500],
             'clf__random_state': [42]
         }, cv=10)
@@ -182,7 +218,7 @@ for classifier in clfs:
     # plot
     plt.figure(figsize=(8, 8))
     plt.scatter(params, means)
-    plt.plot(params, means)
+    #plt.plot(params, means)
     plt.axhline(y=best_mean + best_stdev, color='red')
     plt.axhline(y=best_mean - best_stdev, color='red')
     plt.plot(best_param, best_mean, 'or')
