0033e93e98969cc261c5baeab48b9a0896338cae
Anna <hyyshao@gmail.com>
2020-03-11 22:53:22 -0400

    try to add graphs

--- 20newsgroups.py
+++ 20newsgroups.py
@@ -1,4 +1,5 @@
 import numpy as np
+from Tools.scripts.dutree import display
 from sklearn.datasets import fetch_20newsgroups
 from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.feature_extraction.text import TfidfTransformer
@@ -16,19 +17,21 @@ from sklearn.model_selection import GridSearchCV
 import matplotlib.pyplot as plt
 import pandas as pd
 
-
-
 # Get training data from the 20 news group dataset
-twenty_train = fetch_20newsgroups(subset='train', shuffle=True, random_state=42, remove=(['headers', 'footers', 'quotes']))
-#print(twenty_train.target_names)
+twenty_train = fetch_20newsgroups(subset='train', shuffle=True, random_state=42,
+                                  remove=(['headers', 'footers', 'quotes']))
+# print(twenty_train.target_names)
 
-#plot dataset(a problem with dataset)
-'''
-df = pd.DataFrame(twenty_train.data, columns=twenty_train.feature_names)
-fig = plt.figure(figsize=(8,6))
-df.groupby('target_names').Consumer_complaint_narrative.count().plot.bar(ylim=0)
-plt.show()
-'''
+# plot dataset
+# Feature distribution plot
+plt.figure(1)
+plt.hist(twenty_train.target, rwidth=.2, bins=21, range=(-.5, 20.5))
+plt.xlabel("Features", fontsize=40)
+plt.ylabel("Count", fontsize=40)
+plt.title("Feature distribution in the 20 news-group dataset", fontsize=40)
+plt.xticks(range(20), twenty_train.target_names)
+plt.xticks(rotation=30)
+
 
 # Tokenize text by indexing each word in each document and associate it with its occurrence
 count_vect = CountVectorizer()
@@ -38,28 +41,28 @@ X_train_counts = count_vect.fit_transform(twenty_train.data)
 tfidf_transformer = TfidfTransformer()
 X_train_tfidf = tfidf_transformer.fit_transform(X_train_counts)
 
-#test set
+# test set
 twenty_test = fetch_20newsgroups(subset='test', shuffle=True, random_state=42)
 docs_test = twenty_test.data
 print("Analyzing 20 News Groups dataset .......................................................")
-#fit data with different models using pipelines and forloop
+# fit data with different models using pipelines and forloop
 
 pipeline = Pipeline([
- ('vect', CountVectorizer()),
- ('tfidf', TfidfTransformer()),
- ('clf', LogisticRegression()) #step2 - classifier
+    ('vect', CountVectorizer()),
+    ('tfidf', TfidfTransformer()),
+    ('clf', LogisticRegression())  # step2 - classifier
 ])
+
 clfs = []
 
-
-
+clfs.append(LogisticRegression())  # c
 clfs.append(DecisionTreeClassifier())
-clfs.append(LinearSVC())#c
-clfs.append(AdaBoostClassifier()) #learning rate
-clfs.append(RandomForestClassifier())# n_estimators
-clfs.append(LogisticRegression()) #c
+clfs.append(LinearSVC())  # c
+clfs.append(AdaBoostClassifier())  # learning rate
+clfs.append(RandomForestClassifier())  # n_estimators
 
 
+'''
 best_model = ""
 best_acc = 0
 best_score = 0
@@ -81,44 +84,69 @@ for classifier in clfs:
     for key, values in scores.items():
             print(model_name+" "+ key,' mean ', values.mean())
             print(model_name+" " + key,' std ', values.std())
-
 print("-----------------------------------------------------------------")
 print("Best Model is " + best_model)
 print("Best Accuracy: ", best_acc)
 print("Best score mean: ", best_score)
+'''
 
-
-#find best parameters
+# find best parameters
 best_model = ""
 best_acc = 0
 best_sc = 0
 print("Tuning hyperparameters for the models:..............................................")
 for classifier in clfs:
     pipeline.set_params(clf=classifier)
-    if(isinstance(classifier, LinearSVC) ):
-      cv_grid = GridSearchCV(pipeline, param_grid={
-      'clf__C': np.linspace(0.01, 1.2, 10) #around 0.406
-     }, cv = 3)
-    elif (isinstance(classifier, LogisticRegression)  ):
+    param_name = ""
+    if (isinstance(classifier, LinearSVC)):
         cv_grid = GridSearchCV(pipeline, param_grid={
-            'clf__C': [700, 1000] ,# between 500 to 1000
-            'clf__max_iter': [1000000]
-        }, cv = 3)
+            #'clf__penalty': ['l1', 'l2'],
+            #'clf__loss': ['hinge', 'squared_hinge'],
+            'clf__dual': [False],
+            'clf__C': [0.01, 0.1, 1],#0.7?
+            'clf__random_state': [42]
+        }, cv=3)
+        param_name = "C"
+    elif (isinstance(classifier, LogisticRegression)):
+        cv_grid = GridSearchCV(pipeline, param_grid={
+            #'clf__penalty': ['l1', 'l2'],
+            #'clf__dual': [False],
+            'clf__C': [0.01, 0.1] #1000
+            #'clf__solver': ['liblinear', 'saga'],
+            #'clf__max_iter':[4000],
+            #'clf__random_state': [42]
+        }, cv=3)
+        param_name = "C"
     elif isinstance(classifier, AdaBoostClassifier):
-      cv_grid = GridSearchCV(pipeline, param_grid={
-      'clf__learning_rate': np.linspace(0.01, 1, 5)#around 0.72
-      #'clf__n_estimators': [200,400,1000] # try it after finding best lr
-     }, cv = 3)
+        cv_grid = GridSearchCV(pipeline, param_grid={
+            #'clf__n_estimators': [50, 100, 500],
+            'clf__learning_rate': [0.01, 0.1, 1.],#0.5
+            'clf__random_state': [42]
+            # 'clf__n_estimators': [200,400,1000] # try it after finding best lr
+        }, cv=10)
+        param_name = "learning_rate"
     elif isinstance(classifier, RandomForestClassifier):
-      cv_grid = GridSearchCV(pipeline, param_grid={
-      'clf__n_estimators': [200,400,1000] #1000 is best 
-      #'clf_max_depth': [10, 20, 30, 40, 50, 60, 70, 80, 90, 100, None] #try it later
-     }, cv = 3)
+        cv_grid = GridSearchCV(pipeline, param_grid={
+            'clf__n_estimators': [10, 50, 100],
+            #'clf__criterion': ['gini', 'entropy'],
+            #'clf__max_features': ['auto', 'log2'],
+            #'clf__max_depth': [None, 10, 100],
+            #'clf__max_leaf_nodes': [None, 100, 500],
+            'clf__random_state': [42]
+            # 'clf_max_depth': [10, 20, 30, 40, 50, 60, 70, 80, 90, 100, None] #try it later
+        }, cv=10)
+        param_name = "n_estimators"
     elif isinstance(classifier, DecisionTreeClassifier):
         cv_grid = GridSearchCV(pipeline, param_grid={
-            'clf__max_depth': np.linspace(1, 10, 10, endpoint=True)
-        }, cv=3)
-    else: continue
+            #'clf__criterion': ['gini', 'entropy'],
+            #'clf__max_depth': [None, 10, 100],
+            'clf__max_leaf_nodes': [None, 100, 500],
+            'clf__random_state': [42]
+        }, cv=10)
+        param_name = "max_leaf_nodes"
+    else:
+        continue
+
     cv_grid.fit(twenty_train.data, twenty_train.target)
     best_par = cv_grid.best_params_
     best_estimator = cv_grid.best_estimator_
@@ -127,8 +155,8 @@ for classifier in clfs:
     print("-----------------------------------------------------------------")
     print(model_name)
     print(model_name + " best parameter: " + str(best_par))
-    print(model_name + " best estimator: "+ str(best_estimator.steps[2]))
-    print(model_name + " best CV score: "+ str(best_score))
+    print(model_name + " best estimator: " + str(best_estimator.steps[2]))
+    print(model_name + " best CV score: " + str(best_score))
     y_predict = cv_grid.predict(docs_test)
     accuracy = accuracy_score(twenty_test.target, y_predict)
     print('Accuracy of the best ' + model_name + 'after CV is %.6f%%' % (accuracy * 100))
@@ -137,8 +165,40 @@ for classifier in clfs:
         best_sc = best_score
         best_model = model_name
 
+
+
+
+    #plot results
+    cv_results = cv_grid.cv_results_
+    display(pd.DataFrame(cv_results) \
+            .sort_values(by='rank_test_score').head(2))
+
+    scores_df = pd.DataFrame(cv_results).sort_values(by='rank_test_score')
+    scores_df = scores_df.sort_values(by='param_' + param_name)
+    means = scores_df['mean_test_score']
+    stds = scores_df['std_test_score']
+    best_row = scores_df.iloc[0, :]
+    params = scores_df['param_' + param_name]
+    best_mean = best_row['mean_test_score']
+    best_stdev = best_row['std_test_score']
+    best_param = best_row['param_' + param_name]
+
+    # plot
+    plt.figure(figsize=(8, 8))
+    plt.errorbar(params, means, yerr=stds)
+    plt.axhline(y=best_mean + best_stdev, color='red')
+    plt.axhline(y=best_mean - best_stdev, color='red')
+    plt.plot(best_param, best_mean, 'or')
+
+    plt.title(param_name + " vs Score\nBest Score {:0.5f}".format(best_score))
+    plt.xlabel(param_name)
+    plt.ylabel('Score')
+    plt.show()
+
+
 print("-----------------------------------------------------------------")
 print("After Tuning hyperparameters the best Model is " + best_model)
 print("Best Accuracy: ", best_acc)
-print("Best score mean: ", best_score)
+print("Best score mean: ", best_sc)
 
+plt.show()
--- IMDB.py
+++ IMDB.py
@@ -1,3 +1,5 @@
+import re
+import matplotlib.pyplot as plt
 import sklearn.linear_model
 import numpy as np
 from sklearn.datasets import fetch_20newsgroups
@@ -21,12 +23,13 @@ from sklearn.ensemble import RandomForestClassifier
 from sklearn.model_selection import cross_validate
 from sklearn.model_selection import GridSearchCV
 
-
 # Get the filenames from the training and testing datasets
 train_pos = os.listdir(Path("aclImdb/train/pos"))
 train_neg = os.listdir(Path("aclImdb/train/neg"))
 test_pos = os.listdir(Path("aclImdb/test/pos"))
 test_neg = os.listdir(Path("aclImdb/test/neg"))
+
+grade = [0] * 11
 
 # Create the training and test sets as lists of strings
 train_data = []
@@ -35,13 +38,32 @@ for file in train_neg:
     f = open(path, encoding="utf8")
     train_data.append(f.read())
     f.close()
-    
+
+    # Find given grade
+    a = re.search(r"_\d\.", file)
+    a = int(a.group()[1:-1])
+    grade[a] += 1
+
 for file in train_pos:
     path = os.path.join(Path("aclImdb/train/pos"), file)
     f = open(path, encoding="utf8")
     train_data.append(f.read())
     f.close()
 
+    # Find given grade
+    a = re.search(r"_\d+\.", file)
+    # print(a.group())
+    a = int(a.group()[1:-1])
+    grade[a] += 1
+
+# Feature distribution plot
+plt.figure(1)
+# plt.hist(grade, rwidth = .2, bins = 11, range = (.5,10.5))
+plt.bar(range(1, 11), grade[1:])
+plt.xlabel("Rating", fontsize=40)
+plt.ylabel("Count", fontsize=40)
+plt.title("Grade distribution in the IMDB dataset", fontsize=40)
+plt.show()
 
 test_data = []
 for file in test_neg:
@@ -55,12 +77,10 @@ for file in test_pos:
     f = open(path, encoding="utf8")
     test_data.append(f.read())
     f.close()
-    
-
 
 # Create target lists with neg = 0 and pos = 1 (first half is neg, second half is pos)
-train_target = [i//len(train_pos) for i in range(len(train_data))]
-test_target = [i//len(test_pos) for i in range(len(test_data))]
+train_target = [i // len(train_pos) for i in range(len(train_data))]
+test_target = [i // len(test_pos) for i in range(len(test_data))]
 
 # Shuffle positive and negative instances in both training and testing sets to randomize order
 # The seed ensures that each pair of lists is shuffled the same way
@@ -74,12 +94,11 @@ random.shuffle(test_data)
 random.seed(1)
 random.shuffle(test_target)
 
-
 # Make all lists numpy arrays
-train_data = np.array(train_data)
-train_target = np.array(train_target)
-test_data = np.array(test_data)
-test_target = np.array(test_target)
+# train_data = np.array(train_data)
+# train_target = np.array(train_target)
+# test_data = np.array(test_data)
+# test_target = np.array(test_target)
 
 
 # Tokenize text by indexing each word in each document and associate it with its occurrence
@@ -90,40 +109,42 @@ X_train_counts = count_vect.fit_transform(train_data)
 tfidf_transformer = TfidfTransformer()
 X_train_tfidf = tfidf_transformer.fit_transform(X_train_counts)
 
+# print(train_data[5])
+# print(test_data[2])
 
-#print(train_data[5])
-#print(test_data[2])
-#print(X_train_tfidf.shape)
+# print(X_train_tfidf.shape)
 
-print("Analyzing IMDB dataset .......................................................")
-#fit data with different models using pipelines and forloop
+
+# test set
+twenty_test = fetch_20newsgroups(subset='test', shuffle=True, random_state=42)
+docs_test = twenty_test.data
+print("Analyzing 20 News Groups dataset .......................................................")
+# fit data with different models using pipelines and forloop
 
 pipeline = Pipeline([
- ('vect', CountVectorizer()),
- ('tfidf', TfidfTransformer()),
- ('clf', LogisticRegression()) #step2 - classifier
+    ('vect', CountVectorizer()),
+    ('tfidf', TfidfTransformer()),
+    ('clf', LogisticRegression())  # step2 - classifier
 ])
 clfs = []
 
-
-
 clfs.append(DecisionTreeClassifier())
-clfs.append(LinearSVC())#c
-clfs.append(AdaBoostClassifier()) #learning rate
-clfs.append(RandomForestClassifier())# n_estimators
-clfs.append(LogisticRegression()) #c
+clfs.append(LinearSVC())  # c
+clfs.append(AdaBoostClassifier())  # learning rate
+clfs.append(RandomForestClassifier())  # n_estimators
+clfs.append(LogisticRegression())  # c
 
-
+'''
 best_model = ""
 best_acc = 0
 best_score = 0
 print("Test for different models:................................................")
 for classifier in clfs:
     pipeline.set_params(clf = classifier)
-    pipeline.fit(train_data, train_target)
-    predicted = pipeline.predict(test_data)
-    accuracy = np.mean(predicted == test_target)
-    scores = cross_validate(pipeline,train_data, train_target)
+    pipeline.fit(twenty_train.data, twenty_train.target)
+    predicted = pipeline.predict(docs_test)
+    accuracy = np.mean(predicted == twenty_test.target)
+    scores = cross_validate(pipeline,twenty_train.data, twenty_train.target)
     model_name = type(classifier).__name__
     if(accuracy > best_acc):
         best_acc = accuracy
@@ -135,45 +156,67 @@ for classifier in clfs:
     for key, values in scores.items():
             print(model_name+" "+ key,' mean ', values.mean())
             print(model_name+" " + key,' std ', values.std())
-
 print("-----------------------------------------------------------------")
 print("Best Model is " + best_model)
 print("Best Accuracy: ", best_acc)
 print("Best score mean: ", best_score)
+'''
 
-
-#find best parameters
+# find best parameters
 best_model = ""
 best_acc = 0
 best_sc = 0
 print("Tuning hyperparameters for the models:..............................................")
 for classifier in clfs:
     pipeline.set_params(clf=classifier)
-    if(isinstance(classifier, LinearSVC) ):
-      cv_grid = GridSearchCV(pipeline, param_grid={
-      'clf__C': np.linspace(0.01, 1.2, 10) #around 0.406
-     }, cv = 3)
-    elif (isinstance(classifier, LogisticRegression)  ):
+
+    if (isinstance(classifier, LinearSVC)):
         cv_grid = GridSearchCV(pipeline, param_grid={
-            'clf__C': [700, 1000] ,# between 500 to 1000
-            'clf__max_iter': [1000000]
-        }, cv = 3)
+            'clf__penalty': ['l1', 'l2'],
+            'clf__loss': ['hinge', 'squared_hinge'],
+            'clf__dual': [False],
+            'clf__C': [0.01, 0.1, 1, 10, 100, 1000],
+            'clf__random_state': [42]
+        }, cv=10)
+    elif (isinstance(classifier, LogisticRegression)):
+        cv_grid = GridSearchCV(pipeline, param_grid={
+            'clf__penalty': ['l1', 'l2'],
+            'clf__dual': [False],
+            'clf__C': [0.01, 0.1, 1, 10, 100, 1000],
+            'clf__solver': ['liblinear', 'saga'],
+            'clf__random_state': [42]
+        }, cv=10)
     elif isinstance(classifier, AdaBoostClassifier):
-      cv_grid = GridSearchCV(pipeline, param_grid={
-      'clf__learning_rate': np.linspace(0.01, 1, 5)#around 0.72
-      #'clf__n_estimators': [200,400,1000] # try it after finding best lr
-     }, cv = 3)
-    elif isinstance(classifier, RandomForestClassifier):
-      cv_grid = GridSearchCV(pipeline, param_grid={
-      'clf__n_estimators': [200,400,1000] #1000 is best
-      #'clf_max_depth': [10, 20, 30, 40, 50, 60, 70, 80, 90, 100, None] #try it later
-     }, cv = 3)
+        cv_grid = GridSearchCV(pipeline, param_grid={
+            'clf__n_estimators': [50, 100, 500],
+            'clf__learning_rate': [0.01, 0.1, 1., 10., 100.],
+            'clf__random_state': [42]
+
+            # 'clf__n_estimators': [200,400,1000] # try it after finding best lr
+        }, cv=10)
+
+    if isinstance(classifier, RandomForestClassifier):
+        cv_grid = GridSearchCV(pipeline, param_grid={
+            'clf__n_estimators': [10, 50, 100],
+            'clf__criterion': ['gini', 'entropy'],
+            'clf__max_features': ['auto', 'log2'],
+            'clf__max_depth': [None, 10, 100],
+            'clf__max_leaf_nodes': [None, 100, 500],
+            'clf__random_state': [42]
+            # 'clf_max_depth': [10, 20, 30, 40, 50, 60, 70, 80, 90, 100, None] #try it later
+        }, cv=10)
+
     elif isinstance(classifier, DecisionTreeClassifier):
         cv_grid = GridSearchCV(pipeline, param_grid={
-            'clf__max_depth': np.linspace(1, 10, 10, endpoint=True)
-        }, cv=3)
-    else: continue
-    cv_grid.fit(train_data, train_target)
+            'clf__criterion': ['gini', 'entropy'],
+            'clf__max_depth': [None, 10, 100],
+            'clf__max_leaf_nodes': [None, 100, 500],
+            'clf__random_state': [42]
+        }, cv=10)
+    else:
+        continue
+
+    cv_grid.fit(twenty_train.data, twenty_train.target)
     best_par = cv_grid.best_params_
     best_estimator = cv_grid.best_estimator_
     best_score = cv_grid.best_score_
@@ -181,10 +224,10 @@ for classifier in clfs:
     print("-----------------------------------------------------------------")
     print(model_name)
     print(model_name + " best parameter: " + str(best_par))
-    print(model_name + " best estimator: "+ str(best_estimator.steps[2]))
-    print(model_name + " best CV score: "+ str(best_score))
-    y_predict = cv_grid.predict(test_data)
-    accuracy = accuracy_score(test_target, y_predict)
+    print(model_name + " best estimator: " + str(best_estimator.steps[2]))
+    print(model_name + " best CV score: " + str(best_score))
+    y_predict = cv_grid.predict(docs_test)
+    accuracy = accuracy_score(twenty_test.target, y_predict)
     print('Accuracy of the best ' + model_name + 'after CV is %.6f%%' % (accuracy * 100))
     if (accuracy > best_acc):
         best_acc = accuracy
@@ -194,4 +237,4 @@ for classifier in clfs:
 print("-----------------------------------------------------------------")
 print("After Tuning hyperparameters the best Model is " + best_model)
 print("Best Accuracy: ", best_acc)
-print("Best score mean: ", best_score)
+print("Best score mean: ", best_sc)
